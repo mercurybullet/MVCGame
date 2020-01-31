e407646361abd74dce63d7be0f3dbf2840b6f9f0
Xiwen Yu <xiwyu@microsoft.com>
2020-01-31 16:45:38 +0800

    add WinForm view

---
+++ FormView/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormView
+{
+    using MercuryGames.Shared;
+    using MercuryGames.Snake;
+    public partial class Form1 : Form, IView<SnakeTile, Image>
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool ProcessDialogKey(Keys keyData)
+        {
+
+            if (keyData == Keys.Up || keyData == Keys.Down || keyData == Keys.Left || keyData == Keys.Right)
+            {
+                return false;
+            }
+            else
+            {
+                return base.ProcessDialogKey(keyData);
+            }
+        }
+
+        private PictureBox[,] pbArray;
+        private static int tileLength = 32;
+        protected IDictionary<SnakeTile, Image> textureMap;
+
+        private int width;
+        private int height;
+
+        public void InitView(int w, int h)
+        {
+            this.width = w;
+            this.height = h;
+            this.pbArray = new PictureBox[w, h];
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; j < h; j++)
+                {
+                    PictureBox pb = new PictureBox();
+                    this.pbArray[i, j] = pb;
+
+                    pb.Left = i * Form1.tileLength;
+                    pb.Top = j * Form1.tileLength;
+
+                    pb.Height = Form1.tileLength;
+                    pb.Width = Form1.tileLength;
+
+                    pb.Parent = this;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        public void PrintScore(int score)
+        {
+            this.Text = $"当前分数：{score}";
+        }
+
+        public void Clean()
+        {
+            foreach (PictureBox pb in this.pbArray)
+            {
+                pb.Image = this.textureMap[SnakeTile.Blank];
+            }
+        }
+
+        public void UpdateTile(Point p, SnakeTile type)
+        {
+            this.UpdateTile(p.X, p.Y, type);
+        }
+
+        public void UpdateTile(int x, int y, SnakeTile type)
+        {
+            PictureBox pb = this.pbArray[x, this.height - y - 1];
+            pb.Image = this.textureMap[type];
+        }
+
+        public void DrawTileMap(IReadOnlyTileMap<SnakeTile> map)
+        {
+            for (int i = 0; i < map.Width; i++)
+            {
+                for (int j = 0; j < map.Height; j++)
+                {
+                    this.UpdateTile(i, j, map[i, j]);
+                }
+            }
+        }
+
+        public void SetTextureMap(IDictionary<SnakeTile, Image> map)
+        {
+            this.textureMap = map;
+        }
+    }
+}
---
+++ FormView/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormView
+{
+    using System.Drawing;
+    using MercuryGames.Snake;
+
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var model = new SnakeModel(10, 10);
+            Form1 view = new Form1();
+            view.InitView(10, 10);
+            view.SetTextureMap(new Dictionary<SnakeTile, Image>()
+            {
+                { SnakeTile.Blank, Image.FromFile(@"C:\Users\xiwyu\Pictures\blank.bmp")},
+                { SnakeTile.Snake, Image.FromFile(@"C:\Users\xiwyu\Pictures\snake.bmp")},
+                { SnakeTile.Food, Image.FromFile(@"C:\Users\xiwyu\Pictures\food.bmp")},
+                { SnakeTile.Grass, Image.FromFile(@"C:\Users\xiwyu\Pictures\grass.bmp")}
+            });
+            view.KeyPreview = true;
+
+            var controller = new WinFormController<SnakeTile>(model, view);
+            view.KeyDown += controller.HandelUserInput;
+            controller.InitView();
+            controller.BindSelfToTimer(view.Timer1);
+
+            Application.Run(view);
+
+        }
+    }
+}
